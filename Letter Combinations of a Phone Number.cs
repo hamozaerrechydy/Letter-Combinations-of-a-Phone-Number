2934419a4c5adf639bf846c32acc6336416d0868
hamozaerrechydy <107209152+hamozaerrechydy@users.noreply.github.com>
2022-10-22 12:21:41 +0100

    Create Letter Combinations of a Phone Number.cs

---
+++ Letter Combinations of a Phone Number.cs
@@ -0,0 +1,20 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+
+    int zero = 0; 
+    int one = 1; 
+    string two = "a b c"; 
+    string three = "d e f"; 
+    string four = "g h i";
+    string five = "j k l";
+    string six = "m n o";
+    string seven = "p q r s";
+    string eight = "t u v";
+    string nine = "w x y z";
+
+    // print the value you want 
+    Console.WriteLine (nine);
+  }
+}
